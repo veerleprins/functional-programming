93a45f0c32ba4c3ea404b8ccfe5bbda61a423d3d
veerleprins <veerleprins@hotmail.com>
2020-10-30 11:22:58 +0100

    Updated code with clean workflow and more functional functions

--- my-local-server/js/script.js
+++ my-local-server/js/script.js
@@ -1,57 +1,101 @@
 // Global vars:
 const regx = new RegExp(/rgb/g);
-const endpoint = '../data/userData.json'
+const endpoint = '../data/userData.json';
+const selectedColumn = 'oogKleur';
 const colourObject = {"blauw": "4682b4", "bruin": "603101", "lichtblauw": "add8e6", "groen": "008000"};
 
-fetchJSONdata().then(fullData => {
-  console.log('Data is loaded!');
-  let allEyeColours = fullData.map((data) => {
-    return replaceHashToLower(data.oogKleur);
-  });
+// Retrieve and clean all data:
+fetchAllData(endpoint)
+  .then(fullData => {
+    console.log('Data is loaded!');
+    const eyeColorArray = getColumn(fullData, selectedColumn);
+    const cleanedEyeColorArray = cleanEyeColumn(eyeColorArray);
+    console.log(cleanedEyeColorArray);
+})
 
-  let rgbColourCode = allEyeColours.filter((element) => {
-    // This filter method checks in the total array if there is a rgb i.s.o. hex:
-    return element.match(regx);
-  });
+//ALL THE FUNCTIONS:
 
-  console.log(rgbColourCode);
+// This asynchronous function gets all data from the url and 
+// returns a transformed json file (without all headers etc.).
+async function fetchAllData (url) {
+  const response = await fetch(url);
+  return await response.json();
+};
 
-  let hexArray = replaceRGBToHex(rgbColourCode);
-  console.log(hexArray[0]);
+// This function calls all functions to clean up the eye color 
+// column and returns the cleaned array.
+function cleanEyeColumn(eyeColorArray) {
+  const removedHash = replaceData(eyeColorArray, /#/g, "");
+  const removedSpace = replaceData(removedHash, " ", "");
+  let lowData = lowerData(removedSpace);
+  let rgbArray = getRGB(lowData);
+  replaceToHex(rgbArray, lowData);
+  textToHex(lowData);
+  return lowData;
+}
 
-  let index = allEyeColours.indexOf(rgbColourCode[0]);
-  if (index !== -1) {
-    allEyeColours[index] = hexArray[0];
-  }
-  console.log(index);
-  console.log(allEyeColours);
-
-  allEyeColours.forEach(color =>{
+// This function changes any color in text to a hex code.
+function textToHex(arr) {
+  arr.forEach(color => {
     for (let key in colourObject) {
       if (color == key) {
-        let index = allEyeColours.indexOf(color);
+        let index = arr.indexOf(color);
         if (index !== -1) {
-          allEyeColours[index] = colourObject[key];
+          arr[index] = colourObject[key];
         }
       }
     }
   })
-  console.log(allEyeColours);
+}
 
+// Source: https://github.com/BVictorB/functional-programming 
+// Code from (Victor Boucher):
+// This function returns an array with values in the given column.
+function getColumn (dataArray, column) {
+  return dataArray.map(item => item[column]);
+}
 
-});
+// This function searches for specific values for each item in an array and replaces 
+// specific values.
+function replaceData (dataArray, search, replaced) {
+  return dataArray.map(item => item.replace(search, replaced));
+}
 
-//All the important functions:
+// This function loops through the array and changes each value to lowercase.
+// He then returns a new array with the adjusted values.
+function lowerData (dataArray) {
+  return dataArray.map(item => item.toLowerCase());
+}
 
-//Getting the data from the JSON file:
-async function fetchJSONdata () {
-  const response = await fetch(endpoint);
-  const data = await response.json();
-  return data;
-};
+// This function filters the array for elements that match the specific regular
+// expression and returns those values in an array.
+function getRGB (dataArray) {
+  return dataArray.filter(element => element.match(regx));
+}
 
-function replaceHashToLower (str) {
-  return str.replace(/#/g, "").replace(" ", "").toLowerCase();
+function replaceToHex(dataArray, totalArray) {
+  let removedDots = replaceData(dataArray, ",", ".");
+  let removedBrackets = replaceData(removedDots, /([()])/g, "");
+  let removedText = replaceData(removedBrackets, "rgb", "");
+  let rgbCodeArray = removedText.map(item => item.split("."));
+  let integerArray = rgbCodeArray.map(item => toIntegers(item));
+  let newArrHex = integerArray.map(item => rgbToHex(item));
+  let indexArray = dataArray.map(item => totalArray.indexOf(item));
+  indexArray.forEach(index => {
+    let count = 0;
+    totalArray[index] = newArrHex[count];
+    count++;
+  });
+}
+
+// This function changes the values in an array from strings to integers.
+function toIntegers(dataArray) {
+  return dataArray.map(arrItem => parseInt(arrItem));
+}
+
+//Source: https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+function rgbToHex (arr) {
+  return componentToHex(arr[0]) + componentToHex(arr[1]) + componentToHex(arr[2]);
 };
 
 //Source: https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
@@ -59,64 +103,3 @@ function componentToHex (char) {
   let hex = char.toString(16);
   return hex.length == 1 ? "0" + hex : hex;
 };
-
-function rgbToHex (arr) {
-  let int1 = parseInt(arr[0]);
-  let int2 = parseInt(arr[1]);
-  let int3 = parseInt(arr[2]);
-  return componentToHex(int1) + componentToHex(int2) + componentToHex(int3);
-};
-
-function replaceRGBToHex (rgbColourCode) {
-  let newItem;
-  let newArray = [];
-  rgbColourCode.forEach((item) => {
-    item = item.replace(".", ",");
-    item = item.replace(/([()])/g, "");
-    item = item.replace("rgb", "");
-    item = item.split(",");
-    newItem = rgbToHex(item);
-    newArray.push(newItem);
-  })
-  return newArray;
-};
-
-// fetchJSONdata().then(fullData => {
-//   console.log('Data is loaded!');
-//   let allEyeColours = fullData.map((data) => {
-//     return replaceHashToLower(data.oogKleur);
-//   });
-
-//   let rgbColourCode = allEyeColours.filter((element) => {
-//     // This filter method checks in the total array if there is a rgb i.s.o. hex:
-//     return element.match(regx);
-//   });
-
-//   console.log(rgbColourCode);
-
-//   let hexArray = replaceRGBToHex(rgbColourCode);
-//   console.log(hexArray[0]);
-
-//   let index = allEyeColours.indexOf(rgbColourCode[0]);
-//   if (index !== -1) {
-//     allEyeColours[index] = hexArray[0];
-//   }
-//   console.log(index);
-//   console.log(allEyeColours);
-
-//   allEyeColours.forEach(color =>{
-//     for (let key in colourObject) {
-//       if (color == key) {
-//         let index = allEyeColours.indexOf(color);
-//         if (index !== -1) {
-//           allEyeColours[index] = colourObject[key];
-//         }
-//       }
-//     }
-//   })
-//   console.log(allEyeColours);
-
-
-// });
-
-
